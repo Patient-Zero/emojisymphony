d8cc2e4407ced9aa77cec776d9a8c8d1bb202194
Charmel Sippio <charmelsippio@gmail.com>
2016-08-05 17:17:10 -0400

    Updated emojiRecord.js file to reset the couter to 0 to ensure it will repaly from the top fo the array.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ delete 'logout' => 'sessions#destroy'
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
